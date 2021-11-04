6f02666ebc915a50948f4f6154e71f4c9480be5f
Alan Zhong <azh248@gmail.com>
2021-11-04 17:50:01 -0400

    intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTesting/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTesting/Intake.java
@@ -3,21 +3,23 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Intake {
 	private DcMotor intakeMotor;
+	private double power;
 
 	public Intake(HardwareMap hardwareMap) {
 		intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         intakeMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		power = 1.25;
 	}
 
 	public void intakeIn() { // intake
-		
+		intakeMotor.setPower(power);
 	}
 
 	public void intakeOut() { // reverse intake in case smth gets stuck
-
+		intakeMotor.setPower(-1 * power);
 	}
 	
 	public void stop() {
-
+		intakeMotor.setPower(0);
 	}
 }
