7e85c97eb3d51cdaab0fef340d584ed650c7275d
Alan Zhong <azh248@gmail.com>
2021-11-07 13:25:04 -0500

    Update Carousel.java

--- TeamCode/src/main/java/mechanisms/Carousel.java
+++ TeamCode/src/main/java/mechanisms/Carousel.java
@@ -9,9 +9,14 @@ public class Carousel {
 	public Carousel(HardwareMap hardwareMap) {
 		carouselMotor = hardwareMap.get(DcMotor.class, "carouselMotor");
         carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		power = 1.0;
 	}
 
+<<<<<<< Updated upstream
 	public void spin() { // spin at speed designated by "power"
+=======
+	public void spin() { // spin at speed designated by "power";
+>>>>>>> Stashed changes
 		carouselMotor.setPowerFloat(power);
 	}
 
