d38028f0acd678df4a62ca389d3c55ff9b0874c3
Alan Zhong <azh248@gmail.com>
2021-11-07 13:38:53 -0500

    Update Carousel.java

--- TeamCode/src/main/java/mechanisms/Carousel.java
+++ TeamCode/src/main/java/mechanisms/Carousel.java
@@ -4,21 +4,24 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Carousel {
 	private DcMotor carouselMotor;
-	public int power;
+	public double power;
 
 	public Carousel(HardwareMap hardwareMap) {
 		carouselMotor = hardwareMap.get(DcMotor.class, "carouselMotor");
         carouselMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-		power = 1;
+		power = 1.0;
 	}
 
-	public void spin(power) { // spin at speed designated by "power"
-		DcMotor.setPowerFloat(power)
+	public void spin() { // spin at speed designated by "power"
+		carouselMotor.setPower(power);
+	}
+
+	public void reverseSpin() {
+		carouselMotor.setPower(-power);
 	}
 
 	public void stop() {
-//idek
-
+		carouselMotor.setPower(0);
 	}
-}//hjkgbczjhfkglh;gjfhdgsf
+}
 
