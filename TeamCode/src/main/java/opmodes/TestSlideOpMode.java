b8cb6ea1f2da51e403a054501bef25b67da00131
Alan Zhong <azh248@gmail.com>
2021-11-11 17:28:51 -0500

    Create TestSlideOpMode.java

---
+++ TeamCode/src/main/java/opmodes/TestSlideOpMode.java
@@ -0,0 +1,96 @@
+package opmodes;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import teamcode.GamepadController;
+
+import teamcode.GamepadController.ToggleButton;
+import teamcode.GamepadController.ButtonState;
+import teamcode.GamepadController.FloatButton;
+
+
+@TeleOp(name="Template: Template OpMode", group="Testing")
+public class TemplateOpMode extends OpMode {
+    // tag is used in logcat logs (Log.d()) to identify where the log is coming from
+    // logcat is basically like System.out.print (standard output) except through adb
+    private static final String TAG = "teamcode.test_opmode"; // put the name of the opmode
+
+    // put any outside classes you need to use here
+    private GamepadController mechanismController;
+	private LinearSlide linearSlide;
+
+
+    // put any measurements here
+    private final double inchesToMM = 25.4; // this is correct
+    private final double toCameraCenter = 1.25; // inches from bottom of logitech c615 to actual camera
+    private final double cameraPlatform = 10.5; // random value
+    private final double cameraHeight = (cameraPlatform + toCameraCenter) * inchesToMM;
+    private static final int fieldLength = 3660; // mm (this is correct)
+
+
+
+    // code to run once when driver hits init on phone
+    @Override
+    public void init() {
+        mechanismController = new GamepadController(gamepad1);
+		linearSlide = new LinearSlide(hardwareMap);
+    }
+
+    // code to loop after init is pressed and before start is pressed
+    @Override
+    public void init_loop() {
+    }
+
+    // code to run once when driver hits start
+    @Override
+    public void start() {
+    }
+
+    // code to loop while opmode is running
+    @Override
+    public void loop() {
+
+        runControls();
+
+
+        // update telemetry at the end of the loop
+        telemetry.update();
+    }
+
+    /**
+     * Updates buttons and does controls when buttons are pressed
+     */
+    public void runControls() {
+
+
+        telemetry.addData("caption", "value");
+
+        // button states need to be updated each loop for controls to work
+        mechanismController.updateButtonStates();
+
+		if (mechanismController.getButtonState(ToggleButton.A) == ButtonState.KEY_DOWN) {
+			linearSlide.level1();
+		}
+
+		if (mechanismController.getButtonState(ToggleButton.B) == ButtonState.KEY_DOWN) {
+			linearSlide.level2();
+		}
+
+		if (mechanismController.getButtonState(ToggleButton.X) == ButtonState.KEY_DOWN) {
+			linearSlide.level3();
+		}
+    }
+
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+
+
+}
