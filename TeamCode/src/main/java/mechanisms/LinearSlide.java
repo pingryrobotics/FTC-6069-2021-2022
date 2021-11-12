f21fbf3196218631528c325e33128605f881de68
bigbluerobotics <bigbluerobotics@gmail.com>
2021-11-12 16:34:03 -0500

    Updated Linear Slide Class and Opmode

--- TeamCode/src/main/java/mechanisms/LinearSlide.java
+++ TeamCode/src/main/java/mechanisms/LinearSlide.java
@@ -15,15 +15,16 @@ public class LinearSlide {
 	public LinearSlide(HardwareMap hardwareMap) {
 		slideMotor = hardwareMap.get(DcMotor.class, "slideMotor");
 		slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // not sure if needed but sets base state to 0
-		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+		//slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		bucketServo = hardwareMap.get(Servo.class, "bucketServo");
 		bucketServo.setDirection(Servo.Direction.FORWARD);
 		bucketServo.scaleRange(0, .4);
-		power = 0.2;
-		level = 0;
+		power = 1;
+		level = 3;
 	}
 
 	public void level1() { // extend linear slide to level appropriate for the bottom level of shipping hub
+		slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // not sure if needed but sets base state to 0
 		if(level == 0) {
 			slideMotor.setTargetPosition(200);
 		}
@@ -36,11 +37,13 @@ public class LinearSlide {
 			slideMotor.setTargetPosition(-400);
 		}
 
+		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		level = 1;
 		 // i have no idea what it should be this is an estimate tho
 	}
 
-	public void level2() { // extend linear slide to level appropriate for the middle level of shipping hub 
+	public void level2() { // extend linear slide to level appropriate for the middle level of shipping hub
+		slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // not sure if needed but sets base state to 0
 		if(level == 0) {
 			slideMotor.setTargetPosition(400);
 		}
@@ -52,11 +55,12 @@ public class LinearSlide {
 		else if(level == 3){
 			slideMotor.setTargetPosition(-200);
 		}
-
+		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		level = 2;
 	}
 
 	public void level3() { // extend linear slide to level appropriate for the top level of shipping hub
+		slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // not sure if needed but sets base state to 0
 		if(level == 0) {
 			slideMotor.setTargetPosition(600);
 		}
@@ -68,11 +72,12 @@ public class LinearSlide {
 		else if(level == 2){
 			slideMotor.setTargetPosition(200);
 		}
-
+		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		level = 3;
 	}
 
 	public void level0(){
+		slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // not sure if needed but sets base state to 0
 		if(level == 1) {
 			slideMotor.setTargetPosition(-200);
 		}
@@ -84,19 +89,18 @@ public class LinearSlide {
 		else if(level == 3){
 			slideMotor.setTargetPosition(-600);
 		}
+		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 		level = 0;
 	}
 
 	public void extend() { // continuously extend linear slide
 		slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		slideMotor.setPower(power);
-		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 	}
 	
 	public void retract() { // continuously retract linear slide
 		slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		slideMotor.setPower(-power);
-		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 	}
 
 	public void dump() { // dump stuff in bucket
@@ -109,5 +113,6 @@ public class LinearSlide {
 
 	public void stop() {
 		slideMotor.setTargetPosition(0); // goes back to base state
+		slideMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 	}
 }
