883b8761c63ac44e3c7dfc2c3da0dc48670ece1a
otaylor2023 <otaylor2023@pingry.org>
2021-11-13 09:43:09 -0500

    changed DriveControl

--- TeamCode/src/main/java/mechanisms/DriveControl.java
+++ TeamCode/src/main/java/mechanisms/DriveControl.java
@@ -310,7 +310,7 @@ public class DriveControl {
         //The 1.5x is a multiplier to make sure the offset is applied 2enough to have an actual effect.
 
         //Commented for now to make drivable (find a new value instead of 300.0 and then uncomment to enable)
-        rotation += (averageGoal - averageVelocity / 120.0);
+//        rotation += (averageGoal - averageVelocity / 120.0);
 
 //        if (rotation < .2 || rotation > -.2) {
 //            rotation = 0;
