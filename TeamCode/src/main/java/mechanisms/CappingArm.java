cf978c39bb608631bd386e8d141262f305e62325
dp2023 <74738050+dp2023@users.noreply.github.com>
2022-01-21 17:59:29 -0500

    Implemented Capping Mechanism

---
+++ TeamCode/src/main/java/mechanisms/CappingArm.java
@@ -0,0 +1,46 @@
+package mechanisms;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class CappingArm {
+    private final Servo armServo;
+    protected final Telemetry telemetry;
+
+    public CappingArm(HardwareMap hardwareMap, Telemetry telemetry) {
+        armServo = hardwareMap.get(Servo.class, "cappingServo");
+        armServo.setDirection(Servo.Direction.FORWARD);
+        this.telemetry = telemetry;
+//        armServo.scaleRange(0.19, 0.55);
+    }
+
+    public void turnOutwards() {
+        if (armServo.getPosition() < 1) {
+            armServo.setPosition(armServo.getPosition() + .05);
+        } else {
+            armServo.setPosition(1);
+        }
+    }
+    public void turnInwards() {
+        if (armServo.getPosition() > 0) {
+            armServo.setPosition(armServo.getPosition() - .05);
+        } else {
+            armServo.setPosition(0);
+        }
+    }
+
+    public void spinOut() {
+        armServo.setPosition(0);
+    }
+
+    public void spinIn() {
+        armServo.setPosition(1);
+    }
+
+    public double getServoPos() {
+        return armServo.getPosition();
+    }
+
+}
